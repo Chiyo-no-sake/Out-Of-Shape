a84bba27d45d945276896fc14bf914c9c0cec474
Luca Pasini <l.pasini078@gmail.com>
2020-09-28 14:03:04 +0200

    Project Setup

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    
+    private Vector3 _position;
+    private Rigidbody _rigidbody;
+    public float acceleration = 2;
+    public float maxSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _position = transform.localPosition;
+        handleInput();
+        preventExceedMaxSpeed();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.tag == "Atmosphere"){
+            applyRotation(other.gameObject);
+        }
+    }
+
+    private void applyRotation(GameObject atmosphereOwner) {
+        Vector3 gravityVector = _position - atmosphereOwner.transform.localPosition;
+        transform.localRotation = Quaternion.FromToRotation(Vector3.up, gravityVector);
+    }
+
+    private void handleInput(){
+        Vector3 accelVec = new Vector3();
+        if(Input.GetKey(KeyCode.W)){
+            accelVec.z += acceleration;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            accelVec.z -= acceleration;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            accelVec.x -= acceleration;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            accelVec.x += acceleration;
+        }
+
+        _rigidbody.AddRelativeForce(accelVec, ForceMode.Acceleration);
+    }
+
+    private void preventExceedMaxSpeed(){
+        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxSpeed);
+    }
+}
---
+++ Assets/Scripts/WorldGravity.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGravity : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+    private Collider _atmosphere;
+    private Vector3 _position;
+
+    private float gravityConstant = 0.07f;
+    public float terrainDensity = 5.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponentsInChildren<Rigidbody>()[0];
+        _atmosphere = GetComponent<Collider>();
+        _position = transform.localPosition;
+
+        _rigidbody.mass = 4 * Mathf.PI * Mathf.Pow(transform.GetChild(0).transform.localScale.x, 2) * terrainDensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+        {
+            ApplyGravityTo(other.gameObject);
+        }
+    }
+
+    private void ApplyGravityTo(GameObject target)
+    {
+        Rigidbody targetRb = target.GetComponent<Rigidbody>();
+        Vector3 gravityVector = Vector3.Normalize(_position - target.transform.localPosition);
+
+        float m1 = _rigidbody.mass;
+        float m2 = targetRb.mass;
+        Vector3 targetPos = target.transform.localPosition;
+        float dist = Vector3.Distance(_position, targetPos);
+        if (dist != 0)
+        {
+            float gForce = gravityConstant * m1 * m2 / Mathf.Pow(dist, 2);
+            Debug.Log(gForce);
+            Debug.Log(gravityConstant);
+            Debug.Log(m1);
+            Debug.Log(m2);
+            Debug.Log(dist);
+
+            gravityVector *= gForce;
+            targetRb.AddForce(gravityVector);
+        }
+    }
+}
